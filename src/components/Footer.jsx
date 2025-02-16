af7058d3e8242c258d446902bbd217e72095daa4
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-16 21:04:08 +0100

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-8">
+      © 2024 Blog React. Tous droits réservés.
+    </footer>
+  );
+};
+
+export default Footer;
