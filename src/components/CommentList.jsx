bd0584ec9208a97dabf3da8cf7f68d5c9288cb92
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-17 17:05:39 +0100

    CommentList

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -5,14 +5,26 @@ const CommentList = ({ postId }) => {
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    API.get(`/comments/${postId}`).then((res) => setComments(res.data));
+    API.get(`/comments/${postId}`)
+      .then((res) => {
+        console.log("Commentaires récupérés :", res.data);
+        setComments(res.data);
+      })
+      .catch((err) => console.error("Erreur lors de la récupération des commentaires :", err));
   }, [postId]);
 
   return (
-    <div>
-      {comments.map((comment) => (
-        <p key={comment._id}>{comment.content}</p>
-      ))}
+    <div className="mt-4">
+      {comments.length > 0 ? (
+        comments.map((comment) => (
+          <div key={comment._id} className="border p-2 my-2 rounded">
+            <p className="text-sm text-gray-600">{comment.userId.username}</p>
+            <p>{comment.content}</p>
+          </div>
+        ))
+      ) : (
+        <p>Aucun commentaire pour le moment.</p>
+      )}
     </div>
   );
 };
